98b1857886a0c0818d7fd2d1fcc36bebab01dfd1
Cristian Javier Alvarez <cjavi26000@hotmail.com>
2018-03-17 12:40:58 -0600

    Codigo del Color
    Implemente Switches del color de los paneles dependiendo de en que estado se encuentre el filosofo.
    
    Esto se encuentra en la clase Principal

--- Proyecto_SO/src/proyecto_so/Principal.java
+++ Proyecto_SO/src/proyecto_so/Principal.java
@@ -1,5 +1,7 @@
 package proyecto_so;
 
+import java.awt.Color;
+
 
 /**
  *
@@ -28,7 +30,105 @@ public class Principal extends javax.swing.JFrame {
         fC.start();
         fD.start();
         fE.start();
+        
+        int idFilosofo = 1;
+                        String estado = "";
+
+        switch(idFilosofo)
+        {
+            case 0:
+                switch(estado)
+                {
+                    case "Hambriento":
+                        EstadoA.setBackground(Color.RED);
+                        break;
+                        
+                    case "Comiendo":
+                        EstadoA.setBackground(Color.GREEN);
+                        break;
+                        
+                        
+                    case "Pensando":
+                        EstadoA.setBackground(Color.BLUE);
+                        break;
+                }
+                break;
+                
+            case 1:
+                switch(estado)
+                {
+                    case "Hambriento":
+                        EstadoB.setBackground(Color.RED);
+                        break;
+                        
+                    case "Comiendo":
+                        EstadoB.setBackground(Color.GREEN);
+                        break;
+                        
+                        
+                    case "Pensando":
+                        EstadoB.setBackground(Color.BLUE);
+                        break;
+                }
+                break;
+                
+            case 2:
+                switch(estado)
+                {
+                    case "Hambriento":
+                        EstadoC.setBackground(Color.RED);
+                        break;
+                        
+                    case "Comiendo":
+                        EstadoC.setBackground(Color.GREEN);
+                        break;
+                        
+                        
+                    case "Pensando":
+                        EstadoC.setBackground(Color.BLUE);
+                        break;
+                }
+                break;
+                
+            case 3:
+                switch(estado)
+                {
+                    case "Hambriento":
+                        EstadoD.setBackground(Color.RED);
+                        break;
+                        
+                    case "Comiendo":
+                        EstadoD.setBackground(Color.GREEN);
+                        break;
+                        
+                        
+                    case "Pensando":
+                        EstadoD.setBackground(Color.BLUE);
+                        break;
+                }
+                break;
+                
+            case 4:
+                switch(estado)
+                {
+                    case "Hambriento":
+                        EstadoE.setBackground(Color.RED);
+                        break;
+                        
+                    case "Comiendo":
+                        EstadoE.setBackground(Color.GREEN);
+                        break;
+                        
+                        
+                    case "Pensando":
+                        EstadoE.setBackground(Color.BLUE);
+                        break;
+                }
+                break;
+        }
     }
+    
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -230,7 +330,10 @@ public class Principal extends javax.swing.JFrame {
 
     private void btnIniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIniciarActionPerformed
       
-      
+      EstadoA.setBackground(Color.RED);
+      EstadoB.setBackground(Color.GREEN);
+      EstadoC.setBackground(Color.BLUE);
+      // Pensando Azul, Comiento Rojo, Hambriento Verde
         
     }//GEN-LAST:event_btnIniciarActionPerformed
 
